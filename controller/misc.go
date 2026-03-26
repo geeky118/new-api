33580abea8611fd03a4cded57ef43d28b8548b4b
FGHsg_ <f1079132971@gmail.com>
2026-03-27 04:31:48 +0800

    fix: force status docs link to local docs route

--- controller/misc.go
+++ controller/misc.go
@@ -115,7 +115,7 @@ func GetStatus(c *gin.Context) {
 		"turnstile_check":             common.TurnstileCheckEnabled,
 		"turnstile_site_key":          common.TurnstileSiteKey,
 		"top_up_link":                 common.TopUpLink,
-		"docs_link":                   operation_setting.GetGeneralSetting().DocsLink,
+		"docs_link":                   "/docs",
 		"quota_per_unit":              common.QuotaPerUnit,
 		// 兼容旧前端：保留 display_in_currency，同时提供新的 quota_display_type
 		"display_in_currency":           operation_setting.IsCurrencyDisplay(),
